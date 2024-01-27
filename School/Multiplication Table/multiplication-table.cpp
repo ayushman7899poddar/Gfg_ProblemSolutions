55eb6acfe66203adb21ed731ef333e1b2d7d1538
ayushman7899poddar <113877684+ayushman7899poddar@users.noreply.github.com>
2024-01-27 11:14:03 +0530

    Added Solution - GfG to GitHub

---
+++ School/Multiplication Table/multiplication-table.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    vector<int> getTable(int N)
+    {
+        int i=1;
+        vector<int>vec;
+        while(i<=10)
+        {
+            vec.push_back(N*i);
+            i++;
+        }
+        return vec;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        vector<int> ans = ob.getTable(N);
+        for(int i=0; i<ans.size(); i++)
+        cout<<ans[i]<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
