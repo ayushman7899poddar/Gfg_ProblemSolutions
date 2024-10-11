f70927019a710ec586deee24d5601fdfecc5bb3d
Ayushman Poddar <113877684+ayushman7899poddar@users.noreply.github.com>
2024-10-11 12:44:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reorganize The Array/reorganize-the-array.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> rearrange(const vector<int>& arr) 
+    {
+        int n = arr.size();
+        
+        vector<int>vec(n,-1);
+        
+        for(int i=0; i<arr.size(); i++)
+        {
+            int val = arr[i];
+            if(val>=0 && val<n)
+            {
+                vec[val] = val;
+            }
+            // vec[val] = val;
+        }
+        
+        return vec;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        vector<int> arr;
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution solution;
+        vector<int> ans = solution.rearrange(arr);
+
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
